7baea24f0f5a3028525c675aed4edffc7d2680a3
zaahiir <smartxoft@outlook.com>
2025-06-26 12:40:55 +0530

    feat
    up

--- ems/.venv/Lib/site-packages/month/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from month.widgets import MonthSelectorWidget
-
-
-class MonthField(forms.DateField):
-    widget = MonthSelectorWidget
--- ems/.venv/Lib/site-packages/month/util.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-PY3 = sys.version_info >= (3,)
-
-if PY3:
-    string_type = str
-else:
-    string_type = basestring
